efe76b7c6317f71a5df5d824fcd816ca93915d5d
Viraj Zaveri <44199181+virajz@users.noreply.github.com>
2025-11-21 14:16:29 +0530

    Remove Inngest

--- src/app/api/inngest/route.ts
+++
@@ -1,9 +0,0 @@
-import { serve } from "inngest/next";
-import { inngest } from "@/inngest/client";
-import { processWhatsAppMessage } from "@/inngest/functions";
-
-// Serve Inngest functions
-export const { GET, POST, PUT } = serve({
-  client: inngest,
-  functions: [processWhatsAppMessage],
-});
